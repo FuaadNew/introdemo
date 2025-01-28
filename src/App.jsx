3954ebc098e00309fa3b12edca08e5d13a53dd05
Fuaad <fuaadmohamoud@gmail.com>
2025-01-28 15:58:49 -0600

    object practices

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,25 @@ const Hello = () => {
   )
 }
 
-const Content = ... {
-  return (
-    <div>
-      <Part .../>
-      <Part .../>
-      <Part .../>
-    </div>
-  )
+const object1 = {
+  name: 'Arto Hellas',
+  age: 35,
+  education: 'PhD',
+}
+
+const object2 = {
+  name: 'Full Stack web application development',
+  level: 'intermediate studies',
+  size: 5,
+}
+
+const object3 = {
+  name: {
+    first: 'Dan',
+    last: 'Abramov',
+  },
+  grades: [2, 3, 5, 3],
+  department: 'Stanford University',
 }
 const App = () => {
   const course = 'Half Stack application development'
