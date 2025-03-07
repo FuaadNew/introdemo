817effb35aa74daf78405ea47d23ac1d1b8fec95
Fuaad <fuaadmohamoud@gmail.com>
2025-03-07 13:50:35 -0600

    fix: correct onClick handler in button component
    Changed the onClick handler from addCounter() to addCounter to prevent the function from being called during render. This fixes the button rendering issue and ensures the counter increments only when clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,14 @@
-const Hello = () => {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
-}
+import { useState } from "react"
 
-const object1 = {
-  name: 'Arto Hellas',
-  age: 35,
-  education: 'PhD',
-}
-
-const object2 = {
-  name: 'Full Stack web application development',
-  level: 'intermediate studies',
-  size: 5,
-}
-
-const object3 = {
-  name: {
-    first: 'Dan',
-    last: 'Abramov',
-  },
-  grades: [2, 3, 5, 3],
-  department: 'Stanford University',
-}
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const [counter,setCounter] = useState(0)
+ 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>a
-  )
-}
-
-
-const footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+      <div>{counter}</div>
+      
+      <button onClick={()=>setCounter(counter+1)}>Plus</button>
+      <button onClick={()=>setCounter(0)}>Reset</button>
     </div>
   )
 }
